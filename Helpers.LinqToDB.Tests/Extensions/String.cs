f4e5e96f63c72a835ee32b5c2cb94df82ef3aa60
Jason Pereira <Jason_Pereira@teamhealth.com>
2022-02-05 23:57:23 -0500

    - Refactored - Reduced - Added tests

---
+++ Helpers.LinqToDB.Tests/Extensions/String.cs
@@ -0,0 +1,148 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JasonPereira84.Helpers.LinqToDB.Tests
+{
+    namespace Extensions
+    {
+        using JasonPereira84.Helpers.Extensions;
+
+        [TestClass]
+        public class Test_String
+        {
+            [TestMethod]
+            public void IsNull()
+            {
+                {
+                    var @string = default(String);
+                    Assert.IsTrue(@string.IsNull());
+                }
+
+                {
+                    var @string = (String)null;
+                    Assert.IsTrue(@string.IsNull());
+                }
+
+                {
+                    var @string = "hello";
+                    Assert.IsFalse(@string.IsNull());
+                }
+            }
+
+            [TestMethod]
+            public void IsEmpty()
+            {
+                {
+                    var @string = default(String);
+                    Assert.ThrowsException<ArgumentNullException>(
+                        () => @string.IsEmpty());
+                }
+
+                {
+                    var @string = "";
+                    Assert.IsTrue(@string.IsEmpty());
+                }
+
+                {
+                    var @string = String.Empty;
+                    Assert.IsTrue(@string.IsEmpty());
+                }
+
+                {
+                    var @string = " ";
+                    Assert.IsFalse(@string.IsEmpty());
+                }
+
+                {
+                    var @string = "hello";
+                    Assert.IsFalse(@string.IsEmpty());
+                }
+            }
+            
+            [TestMethod]
+            public void IsWhiteSpace()
+            {
+                {
+                    var @string = default(String);
+                    Assert.ThrowsException<ArgumentNullException>(
+                        () => @string.IsWhiteSpace());
+                }
+
+                {
+                    var @string = " ";
+                    Assert.IsTrue(@string.IsWhiteSpace());
+                }
+
+                {
+                    var @string = "";
+                    Assert.IsFalse(@string.IsWhiteSpace());
+                }
+
+                {
+                    var @string = "hello";
+                    Assert.IsFalse(@string.IsWhiteSpace());
+                }
+            }
+            
+            [TestMethod]
+            public void IsNullOrEmpty()
+            {
+                {
+                    var @string = default(String);
+                    Assert.IsTrue(@string.IsNullOrEmpty());
+                }
+
+                {
+                    var @string = String.Empty;
+                    Assert.IsTrue(@string.IsNullOrEmpty());
+                }
+
+                {
+                    var @string = "";
+                    Assert.IsTrue(@string.IsNullOrEmpty());
+                }
+
+                {
+                    var @string = " ";
+                    Assert.IsFalse(@string.IsNullOrEmpty());
+                }
+
+                {
+                    var @string = "hello";
+                    Assert.IsFalse(@string.IsNullOrEmpty());
+                }
+            }
+            
+            [TestMethod]
+            public void IsEmptyOrWhiteSpace()
+            {
+                {
+                    var @string = default(String);
+                    Assert.ThrowsException<ArgumentNullException>(
+                        () => @string.IsWhiteSpace());
+                }
+
+                {
+                    var @string = String.Empty;
+                    Assert.IsTrue(@string.IsEmptyOrWhiteSpace());
+                }
+
+                {
+                    var @string = "";
+                    Assert.IsTrue(@string.IsEmptyOrWhiteSpace());
+                }
+
+                {
+                    var @string = " ";
+                    Assert.IsTrue(@string.IsEmptyOrWhiteSpace());
+                }
+
+                {
+                    var @string = "hello";
+                    Assert.IsFalse(@string.IsEmptyOrWhiteSpace());
+                }
+            }
+            
+        }
+    }
+}
